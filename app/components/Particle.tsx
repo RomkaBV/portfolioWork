75b0122f231ea19e2b823eee8c7eed62239cdaea
RomkaBV <barzoromka@gmail.com>
2024-02-26 18:19:43 +0200

    first commit

--- app/components/Particle.tsx
+++ app/components/Particle.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useEffect, useMemo, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 // import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
@@ -24,65 +25,46 @@ const Particle = () => {
     });
   }, []);
 
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
-
-  const options = useMemo(
-    () => ({
-      autoPlay: true,
-      background: {
-        color: {
-          value: "transparent",
-        },
-      },
-      particles: {
-        color: {
-          value: "#fff",
-        },
-        move: {
-          angle: {
-            offset: 0,
-            value: 90,
-          },
-          decay: 0,
-          direction: "top",
-          rondom: true,
-          straight: false,
-          drift: true,
-          enable: true,
-          speed: 1,
-        },
-        number: {
-          density: {
-            enable: true,
-            area: 1000,
-            factor: 1500,
-          },
-          limit: 0,
-          value: 400,
-        },
-        size: {
-          value: 1,
-          random: true,
-          anim: {
-            enable: false,
-            speed: 20,
-            size_min: 0.1,
-            sync: false,
-          },
-        },
-      },
-    }),
-    []
-  );
-
   if (init) {
     return (
       <Particles
         id="tsparticles"
-        particlesLoaded={particlesLoaded}
-        options={options}
+        options={{
+          autoPlay: true,
+          background: {
+            color: {
+              value: "transparent",
+            },
+          },
+          particles: {
+            color: {
+              value: "#fff",
+            },
+            move: {
+              angle: {
+                offset: 0,
+                value: 90,
+              },
+              decay: 0,
+              direction: "top",
+
+              straight: false,
+
+              enable: true,
+              speed: 1,
+            },
+            number: {
+              density: {
+                enable: true,
+              },
+
+              value: 400,
+            },
+            size: {
+              value: 1,
+            },
+          },
+        }}
       />
     );
   }
